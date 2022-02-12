7b13c335a47e18fb720e434e0df90ea0c4609109
ammar <ammarraneez@gmail.com>
2022-02-12 19:17:04 +0530

    feat: add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ["./screens/**/*.{html,tsx,ts}", "App.tsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  corePlugins: require('tailwind-rn/unsupported-core-plugins'),
+}
